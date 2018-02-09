602c505be48db307cdafa8c3a4e54ffbd88ea9f0
Micah Hauge <micahhauge@Micahs-MacBook-Pro.local>
2018-02-09 14:35:18 -0600

    Added gitignore and updated api.py

--- project_api/api.py
+++ project_api/api.py
@@ -1,1 +1,27 @@
+from flask import *
+from flask_sqlalchemy import *
+
+app = FLask(__name__)
+
+
+# configure postgres settings
+POSTGRES = {
+    'user': 'postgres',
+    'pw': '',
+    'db': 'postgres',
+    'host': 'db',
+    'port': '5432',
+}
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://%(user)s:\%(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES
+db = SQLAlchemy(app)
+
+#### MODELS ####
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    public_id = db.Column(db.String(50), unique=True)
+    name = db.Column(db.String(50))
+    password = db.Column(db.String(80))
+    admin = db.Column(db.Boolean)
+
+
 print('hello world')
