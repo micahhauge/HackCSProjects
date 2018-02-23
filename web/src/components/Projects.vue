6e1b66c7b69cac40432ca370ddad911483e30f83
Micah Hauge <micahhauge@Micahs-MacBook-Pro.local>
2018-02-23 14:55:53 -0600

    Got frontend setup working

---
+++ web/src/components/Projects.vue
@@ -0,0 +1,30 @@
+
+<template>
+    <p>
+      test
+    </p>
+</template>
+
+<script>
+import axios from 'axios';
+
+
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      projects: [],
+    }
+  },
+  created() {
+    axios.get('http://localhost:5001/api/project')
+      .then( response => {
+        this.posts = response.data
+        console.log(response.data)
+      })
+      .catch(e => {
+        this.errors.push(e)
+      })
+  }
+}
+</script>
