fb8a05d72d153461d4fc7aba0b89177c4938d16d
Micah Hauge <micahhauge@Micahs-MacBook-Pro.local>
2018-02-23 15:31:11 -0600

    Added basic styling

--- web/src/components/Projects.vue
+++ web/src/components/Projects.vue
@@ -1,5 +1,17 @@
 
 <template>
+  <div class='container'>
+  <h3>Projects</h3>
+  <div v-for="project in projects" class="card border-primary mb-3" style="max-width: 20rem;">
+    <div class="card-header">{{ project.name }}</div>
+    <div class="card-body">
+      <h4 class="card-title">{{project.name}}</h4>
+      <p class="card-text"> {{project.created_date}} </p>
+      <p class="card-text"> {{project.upvote}} </p>
+      <p class="card-text">{{ project.description }}</p>
+    </div>
+  </div>
+  </div>
 </template>
 
 <script>
@@ -16,7 +28,7 @@ export default {
   created() {
     axios.get('http://localhost:5001/api/project')
       .then( response => {
-        this.posts = response.data
+        this.projects = response.data
         console.log(response.data)
       })
       .catch(e => {
@@ -25,3 +37,20 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
